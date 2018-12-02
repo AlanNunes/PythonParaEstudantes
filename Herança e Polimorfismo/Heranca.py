b1d3fba94cd6eb8711e8e7e95186bbe19cd97576
AlanNunes <alann.625@gmail.com>
2018-12-02 13:15:35 -0200

    Ensina Herança e Polimorfismo

---
+++ Herança e Polimorfismo/Heranca.py
@@ -0,0 +1,25 @@
+# Herança é quando se herda características e métodos de uma classe
+# Polimorfismo é o mesmo de herança porém ele altera as características e
+# métodos herdados, o próprio nome 'polimorfismo' já é explicativo
+
+# Exemplo de Herança
+class Bebida:
+    def __init__(self, sabor, capacidade, preco):
+        self.sabor = sabor
+        self.capacidade = preco
+        self.preco = preco
+
+class Suco(Bebida):
+    def __init__(self, sabor, capacidade, preco):
+        Bebida.__init__(self, sabor, capacidade, preco)
+
+class Refrigerante(Bebida):
+    def __init__(self, sabor, capacidade, preco):
+        Bebida.__init__(self, sabor, capacidade, preco)
+
+
+delValle = Suco('morango', '1L', 5.59)
+cocaCola = Refrigerante('Cola', '2L', 5.50)
+
+print(delValle.sabor)
+print(cocaCola.sabor)
---
+++ Herança e Polimorfismo/Polimorfismo.py
@@ -0,0 +1,31 @@
+# Herança é quando se herda características e métodos de uma classe
+# Polimorfismo é o mesmo de herança porém ele altera as características e
+# métodos herdados, o próprio nome 'polimorfismo' já é explicativo
+
+# Exemplo de Polimorfismo
+
+class Bebida:
+    def __init__(self, sabor, capacidade, preco):
+        self.sabor = sabor
+        self.capacidade = preco
+        self.preco = preco
+    def detalhe(self):
+        print('Faz bem para a saúde!')
+
+class Suco(Bebida):
+    def __init__(self, sabor, capacidade, preco):
+        Bebida.__init__(self, sabor, capacidade, preco)
+
+class Refrigerante(Bebida):
+    def __init__(self, sabor, capacidade, preco):
+        Bebida.__init__(self, sabor, capacidade, preco)
+    # Aqui ocorre polimorfismo pois alteramos o método que foi herdado
+    def detalhe(self):
+        print('Faz mal para a saúde!')
+
+
+delValle = Suco('morango', '1L', 5.59)
+cocaCola = Refrigerante('cola', '2L', 5.59)
+# As mensagens que aperecerão devem ser diferentes, pois foi feito polimorfismo
+delValle.detalhe()
+cocaCola.detalhe()
