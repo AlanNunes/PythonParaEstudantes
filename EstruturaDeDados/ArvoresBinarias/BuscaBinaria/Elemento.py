f98bcbb5946f6f8bb977679f174c584b090df707
Alan Nunes da Silva <alann.625@gmail.com>
2019-04-25 20:18:01 -0300

    Update Elemento.py

--- EstruturaDeDados/ArvoresBinarias/BuscaBinaria/Elemento.py
+++ EstruturaDeDados/ArvoresBinarias/BuscaBinaria/Elemento.py
@@ -1,12 +1,7 @@
-class No:
-    def __init__(self):
-        self._elemento = e
-        self._proximo = None
-    def getElemento(self):
-        return self._elemento
-    def setElemento(self, e):
-        self._elemento = e
-    def getProximo(self):
-        return self._proximo
-    def setProximo(self, p):
-        self._proximo = p
+class Elemento:
+    def __init__(self, v):
+        self._valor = v
+    def getValor(self):
+        return self._valor
+    def setValor(self, v):
+        self._valor = v
