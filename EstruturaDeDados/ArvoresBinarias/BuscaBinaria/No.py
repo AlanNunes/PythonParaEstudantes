2f4258f378c85b48bee37603ead06d70ba2b7388
Alan Nunes da Silva <alann.625@gmail.com>
2019-05-09 20:19:15 -0300

    Corrige nome de variáveis

--- EstruturaDeDados/ArvoresBinarias/BuscaBinaria/No.py
+++ EstruturaDeDados/ArvoresBinarias/BuscaBinaria/No.py
@@ -2,14 +2,14 @@ from Elemento import Elemento
 class No:
     def __init__(self):
         self._elemento = Elemento(None)
-        self.filhoEsquerda = None
-        self.filhoDireita = None
+        self._filhoEsquerda = None
+        self._filhoDireita = None
     def getFilhoEsquerda(self):
-        return self.filhoEsquerda
+        return self._filhoEsquerda
     def setFilhoEsquerda(self, n):
-        self.filhoEsquerda = n
+        self._filhoEsquerda = n
     def setfilhoDireita(self, n):
-        self.filhoDireita = n
+        self._filhoDireita = n
     def getFilhoDireita(self):
         return self._filhoDireita
     def getElemento(self):
