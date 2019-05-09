fd23f3dce1f525fd34fa089646c5667f687936dc
Alan Nunes da Silva <alann.625@gmail.com>
2019-05-09 20:23:26 -0300

    Cria programa principal

---
+++ EstruturaDeDados/ArvoresBinarias/BuscaBinaria/Main.py
@@ -0,0 +1,29 @@
+from Elemento import Elemento
+from No import No
+from ArvoreBuscaBinaria import ArvoreBuscaBinaria
+import os
+
+def menu():
+    os.system('cls')
+    print('1 - inserir \n')
+    print('2 - remover \n')
+    print('3 - pré ordem \n')
+    print('4 - em ordem \n')
+    print('0 - termina \n')
+    opcao = int(input('Digite uma opção: '))
+    return opcao
+
+arvoreBinaria = ArvoreBuscaBinaria()
+opcao = 1
+while opcao != 0:
+    if opcao == 1:
+        valor = int(input('Digite um valor: '))
+        arvoreBinaria.insereNo(valor)
+    elif opcao == 2:
+        valor = int(input('Digite um valor: '))
+        arvoreBinaria.remove(valor)
+    elif opcao == 3:
+        arvoreBinaria.preOrdem(arvoreBinaria.getRaiz())
+    elif opcao == 4:
+        arvoreBinaria.emOrdem(arvoreBinaria.getRaiz())
+    opcao = menu()
